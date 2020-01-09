3f2c480f372869fc5331da30175a6e58560f7007
황지혜 <jh.hwang@crscube.co.kr>
2020-01-09 11:29:29 +0900

    Theater의 enter 메서드에서 TicketOffice에 접근하는 모든 코드를 TicketSeller 내부로 캡슐화
    TicketSeller에 sellTo 메서드를 추가, Theater에 있던 로직을 sellTo 메서드로 옮기기

--- src/Theater.java
+++ src/Theater.java
@@ -5,22 +5,18 @@ public class Theater {
         this.ticketSeller = ticketSeller;
     }
 
-// 소극장이 관람객을 맞이할 수 있도록 enter 메서드를 구현
+    /*
+     수정된 Theater 클래스 어디에서도 ticketOffice에 접근하지 않는다.
+     Theater는 ticketOffice가 TicketSeller 내부에 존재한다는 사실을 알지 못한다.
+     단지, ticketSeller가 sellTo 메시지를 이해하고 응답할 수 있다는 사실만 알고 있을 뿐!
+
+     Theater는 오직 ticketSeller의 인터페이스(interface)에만 의존한다.
+     ticketSeller 내부에 ticketOffice 인스턴스를 포함하고 있따는 사실은 구현의 영역에 속한다.
+     객체를 인터페이스와 구현으로 나누고 인터페이스만을 공개하는 것
+        - 객체 사이의 결합도를 낮추고, 변경하기 쉬운 코드를 작성하기 위해 따라야 하는
+        가장 기본적인 설계 원칙
+     */
     public void enter(Audience audience) {
-        // 소극장은 먼저 관람객의 가방 안에 초대장이 들어 있는지 확인한다.
-        // 초대장이 들어 있다면, 이벤트에 당첨된 관람객이므로
-        if (audience.getBag().hasInvitation()) {
-            // 판매원에게서 받은 티켓을 관람객의 가방에 넣어준다.
-            Ticket ticket = ticketSeller.getTicketOffice().getTicket();
-            audience.getBag().setTicket(ticket);
-            // 가방 안에 초대장이 없다면, 티켓을 판매해야 한다.
-        } else {
-            // 소극장은 관람객의 가방에서 티켓 금액만큼을 차감한 후, 매표소에 금액을 증가시킨다.
-            Ticket ticket = ticketSeller.getTicketOffice().getTicket();
-            audience.getBag().minusAmount(ticket.getFee());
-            ticketSeller.getTicketOffice().plusAmount(ticket.getFee());
-            // 소극장은 관람객의 가방 안에 티켓을 넣어줌으로써 관람객의 입장 절차를 끝낸다.
-            audience.getBag().setTicket(ticket);
-        }
+        ticketSeller.sellTo(audience);
     }
 }
--- src/TicketSeller.java
+++ src/TicketSeller.java
@@ -3,13 +3,33 @@
     판매원을 구현한 TicketSeller 클래스는 자신이 일하는 매표소(ticketOffice)을 알고 있어야 한다.
  */
 public class TicketSeller {
+    /*
+     TicketSeller에서 getTicketOffic 메서드가 제거됨
+     -> ticketOffice의 가시성이 private이고, 접근 가능한 퍼블릭 메서드가 존재하지 않는다.
+     -> 외부에서는 ticketOffice에 직접 접근할 수 X!
+     -> TicketSeller는 ticketOffice에서 티켓을 꺼내거나 판매 요금을 적립하는 일을  스스로 수행할 수 밖에 없다!
+
+     - 캡슐화: 개념적으로나 물리적으로 객체 내부의 세부적인 사항을 감추는 것
+     */
     private TicketOffice ticketOffice;
 
     public TicketSeller(TicketOffice ticketOffice) {
         this.ticketOffice = ticketOffice;
     }
 
-    public TicketOffice getTicketOffice() {
-        return ticketOffice;
+//    public TicketOffice getTicketOffice() {
+//        return ticketOffice;
+//    }
+
+    public void sellTo(Audience audience) {
+        if (audience.getBag().hasInvitation()) {
+            Ticket ticket = ticketOffice.getTicket();
+            audience.getBag().setTicket(ticket);
+        } else {
+            Ticket ticket = ticketOffice.getTicket();
+            audience.getBag().minusAmount(ticket.getFee());
+            ticketOffice.plusAmount(ticket.getFee());
+            audience.getBag().setTicket(ticket);
+        }
     }
 }
