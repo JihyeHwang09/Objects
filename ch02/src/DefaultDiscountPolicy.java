90c6e55a583f4b1b3aa6454abc4d87b7eec58be4
황지혜 <jh.hwang@crscube.co.kr>
2020-01-13 12:21:32 +0900

    부모 클래스인 DiscountPolicy에서 할인 조건이 없을 경우, getDiscountAmount()를 호출하지 않는 문제 해결

---
+++ ch02/src/DefaultDiscountPolicy.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public abstract class DefaultDiscountPolicy implements DiscountPolicy {
+    private List<DiscountCondition> conditions = new ArrayList<>();
+
+    // DefaultDiscountPolicy의 생성자는 여러 개의 DiscountCondition 인스턴스를 허용한다.
+    /*
+    이처럼 생성자의 파라미터 목록을 이용해 초기화에 필요한 정보를 전달하도록 강제하면,
+    올바른 상태를 가진 객체의 생성을 보장할 수 있다.
+     */
+    public DefaultDiscountPolicy(DiscountCondition ... conditions) {
+        this.conditions = Arrays.asList(conditions);
+    }
+
+    public Money calculateDiscountAmount(Screening screening) {
+        for (DiscountCondition each : conditions) {
+            // isSatiesfiedBy 메서드는 인자로 전달된 screening이 할인 조건을 만족할 경우 true,
+            // 만족시키지 못할 경우 false를 반환한다.
+            if (each.isSatisfiedBy(screening)) {
+                return getDiscountAmount(screening);
+            }
+        }
+        // 만족하는 할인 조건이 하나도 존재하지 않는다면, 할인 요금을 0원으로 반환한다.
+        return Money.ZERO;
+    }
+    abstract protected Money getDiscountAmount(Screening screening);
+}
+
--- ch02/src/NoneDiscountPolicy.java
+++ ch02/src/NoneDiscountPolicy.java
@@ -1,11 +1,12 @@
-public class NoneDiscountPolicy extends DiscountPolicy {
+public class NoneDiscountPolicy implements DiscountPolicy {
 
     // getDiscountAmount 메서드가 어떤 값을 반환하더라도 상관 없다.
     // 부모 클래스인 DiscountPolicy에서 할인 조건이 없을 경우, getDiscountAmount()를 호출하지 않기 때문
     // -> 해결 방법: DiscountPolicy를 인터페이스로 바꾸고 NoneDiscountPolicy가 DiscountPolicy의
     // getDiscountAmount가 아닌 calculateDiscountAmount() 오퍼레이션을 오버라이딩하도록 변경한다.
+
     @Override
-    protected Money getDiscountAmount(Screening screening) {
+    public Money calculateDiscountAmount(Screening screening) {
         return Money.ZERO;
     }
 }
